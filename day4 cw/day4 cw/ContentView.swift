9987bef23bc7f360528e0ec7c12df4d618680cbe
Fatema179 <109864304+Fatema179@users.noreply.github.com>
2022-08-16 13:17:38 +0300

    day4

---
+++ day4 cw/day4 cw/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  day4 cw
+//
+//  Created by Zeinab H Eldeeb on 16/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var finalGrade = ""
+    @State var gradedes = ""
+    
+    var body: some View {
+        ZStack{
+            Color.blue.opacity(0.3)
+                .ignoresSafeArea()
+            
+            
+            VStack{
+                Text("حاسبة الدرجات")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                Spacer()
+                
+                Image("calc")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                
+                TextField("Enter Your Grade", text: $finalGrade)
+                    .background(.white)
+                    .textFieldStyle(.roundedBorder)
+                Text("احسب درجتي")
+                    .frame(width: 120, height: 40)
+                    .background(.yellow)
+                    .onTapGesture {
+                        if (Double(finalGrade) ?? 0) >= 90 {
+                            gradedes = "امتياز"
+                        }
+                        else{
+                            gradedes = "رسوب"
+                        }
+                    }
+                //
+                Text("اقد حصلت على درجة")
+                
+                Spacer()
+                Text(gradedes)
+                    .font(.largeTitle)
+                
+            }.padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day4 cw/day4 cw/day4_cwApp.swift
@@ -0,0 +1,17 @@
+//
+//  day4_cwApp.swift
+//  day4 cw
+//
+//  Created by Zeinab H Eldeeb on 16/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct day4_cwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
